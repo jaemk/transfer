bb62a743abd339d9932c9e75a757f40623b58ab5
James Kominick <james.kominick@gmail.com>
2017-10-03 23:28:19 -0400

    Add progress bars and result message

--- web/src/components/Delete.js
+++ web/src/components/Delete.js
@@ -19,6 +19,8 @@ class Delete extends Component {
       required: {},
       submitted: false,
       inputOk: false,
+
+      responseStatus: null,
     };
     this.delete = this.delete.bind(this);
   }
@@ -54,12 +56,37 @@ class Delete extends Component {
     const params = {key: this.state.key, deletion_password: Buffer.from(this.state.deletePass).toString('hex')}
     const headers = {headers: {'content-type': 'application/json'}}
     axios.post('/api/upload/delete', params, headers).then(resp => {
-      console.log(resp.data);
+      this.setState({
+        responseStatus: resp.status,
+      });
+    }).catch(err => {
+      console.log(err);
+      this.setState({responseStatus: err.response.status})
     });
   }
 
   render() {
     const disable = this.state.submitted && this.state.inputOk;
+    let message = null;
+    switch (this.state.responseStatus) {
+      case null:
+        break;
+      case 200:
+        message = <div> Success! </div>;
+        break;
+      case 404:
+        message = <div> Error: Upload not found </div>;
+        break;
+      case 401:
+        message = <div> Error: Invalid credentials </div>;
+        break;
+      case 400:
+        message = <div> Error: Bad request </div>;
+        break;
+      default:
+        message = <div> Something went wrong </div>;
+    }
+
     return (
       <div>
         <Form inline onSubmit={this.delete}>
@@ -88,6 +115,12 @@ class Delete extends Component {
             Delete Upload
           </Button>
         </Form>
+        {
+          (message === null)?
+            ''
+            :
+            message
+        }
       </div>
     );
   }
